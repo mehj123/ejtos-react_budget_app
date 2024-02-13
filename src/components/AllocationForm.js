119f5d26447af10537f072ebf4d8103bcd579801
Mehjabeen Begum <mehjabeen.begum@outlook.com>
2024-02-13 14:49:59 -0500

    second

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -11,7 +11,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("You cannot reduce the budget value lower than spending");
                 setCost("");
                 return;
             }
@@ -41,7 +41,7 @@ const AllocationForm = (props) => {
                     <div className="input-group-prepend">
                 <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
                   </div>
-                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                                <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
                         <option defaultValue>Choose...</option>
                         <option value="Marketing" name="marketing"> Marketing</option>
                 <option value="Sales" name="sales">Sales</option>
@@ -49,7 +49,7 @@ const AllocationForm = (props) => {
                 <option value="HR" name="hr">HR</option>
                 <option value="IT" name="it">IT</option>
                 <option value="Admin" name="admin">Admin</option>
-                  </select>
+                  </select>                                                                     
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
                 <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -28,7 +28,16 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
+        <td>{props.cost}
+        <select style={{ background: 'green' }} className="custom-select alert-success" id="inputGroupSelect01" >
+                        <option style={{ background: 'green' }}  defaultValue>Currency(£ Pound)</option>
+                        <option  style={{ background: 'green' }} value="Marketing" name="marketing"> $ Dollar</option>
+                <option value="Sales" name="sales">£ Pound</option>
+                <option style={{ background: 'green' }} value="Finance" name="finance">€ Euro</option>
+                <option style={{ background: 'green' }} value="HR" name="hr">₹ Ruppee</option>
+                  </select> 
+        
+        </td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
